af68a9ada3ee22ccd7e197714a7ea7a8295638f0
Dewi Nayati <dnayati@gmail.com>
2024-01-26 11:04:39 +0700

    nambahin template lte

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('layouts.head')
+
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="{{ asset("assets/admin_lte/dist/img/AdminLTELogo.png") }}" alt="AdminLTELogo" height="60" width="60">
+  </div>
+
+  <!-- Navbar -->
+  @include('layouts.navbar')
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  @include('layouts.sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  @include('layouts.header')
+
+  <!--start content-->
+    <main class="page-content">
+      @yield('container')
+    </main>
+  <!--end page main-->
+  
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+@include('layouts.foot')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login', function () {
+    return view('login/login');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
